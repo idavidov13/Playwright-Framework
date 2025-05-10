7dd5aa5e35977849ebac1169e529105255e16bbe
Ivan Davidov <ivan.davidov@primeholding.com>
2025-05-10 22:29:44 +0300

    implement Article Page

--- fixtures/pom/page-object-fixture.ts
+++ fixtures/pom/page-object-fixture.ts
@@ -1,11 +1,14 @@
 import { test as base } from '@playwright/test';
 import { HomePage } from '../../pages/clientSite/homePage';
 import { NavPage } from '../../pages/clientSite/navPage';
+import { ArticlePage } from '../../pages/clientSite/articlePage';
 
 export type FrameworkFixtures = {
     homePage: HomePage;
 
     navPage: NavPage;
+
+    articlePage: ArticlePage;
 };
 
 export const test = base.extend<FrameworkFixtures>({
@@ -15,6 +18,10 @@ export const test = base.extend<FrameworkFixtures>({
 
     navPage: async ({ page }, use) => {
         await use(new NavPage(page));
+    },
+
+    articlePage: async ({ page }, use) => {
+        await use(new ArticlePage(page));
     },
 });
 
