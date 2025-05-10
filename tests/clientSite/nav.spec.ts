7b0066a5453e760d3b43a00902b5a70ead09940d
Ivan Davidov <ivan.davidov@primeholding.com>
2025-05-10 21:19:26 +0300

    add test for Nav Page

---
+++ tests/clientSite/nav.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '../../fixtures/pom/test-options';
+
+test.describe('Verify Home Page', () => {
+    test.beforeEach(async ({ homePage }) => {
+        await homePage.navigateToHomePage();
+    });
+
+    test(
+        'Verify Successful Log In/Log Out',
+        { tag: '@Smoke' },
+        async ({ navPage }) => {
+            await test.step('Perform Log In', async () => {
+                await navPage.logIn(process.env.EMAIL!, process.env.PASSWORD!);
+            });
+
+            await test.step('Perform Log Out', async () => {
+                await navPage.logOut();
+            });
+        }
+    );
+
+    test(
+        'Verify Navigation from Navigation Bar',
+        { tag: '@Sanity' },
+        async ({ homePage, navPage, page }) => {
+            await test.step('Navigate to Sign In Page', async () => {
+                await navPage.navigateToSignInPage();
+            });
+
+            await test.step('Navigate to Home Page by Icon', async () => {
+                await navPage.navigateToHomePageByIcon();
+            });
+
+            await test.step('Navigate to Sign Up Page', async () => {
+                await navPage.navigateToSingUpPage();
+            });
+
+            await test.step('Navigate to Home Page by Home Link', async () => {
+                await navPage.navigateToHomePage();
+            });
+        }
+    );
+});
