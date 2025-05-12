8fcd13bb6aae1918eee5273546d0e2daba29cdb8
Ivan Davidov <ivan.davidov@primeholding.com>
2025-05-12 12:18:13 +0300

    change Tag

--- tests/clientSite/failingTest.spec.ts
+++ tests/clientSite/failingTest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../../fixtures/pom/test-options';
 
-test('Failing Test', { tag: '@Smoke' }, async ({ homePage }) => {
+test('Failing Test', { tag: '@Regressions' }, async ({ homePage }) => {
     await homePage.navigateToHomePageUser();
     expect(2).toEqual(3);
 });
