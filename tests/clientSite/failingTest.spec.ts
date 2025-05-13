ef361ac4eb4e365547168d69f927870d9c1f85f6
Ivan Davidov <ivan.davidov@primeholding.com>
2025-05-13 16:17:59 +0300

    simulate failing tests for different jobs

--- tests/clientSite/failingTest.spec.ts
+++ tests/clientSite/failingTest.spec.ts
@@ -1,6 +1,16 @@
 import { test, expect } from '../../fixtures/pom/test-options';
 
-test('Failing Test', { tag: '@Regressions' }, async ({ homePage }) => {
+test('Failing Sanity Test', { tag: '@Sanity' }, async ({ homePage }) => {
+    await homePage.navigateToHomePageUser();
+    expect(2).toEqual(3);
+});
+
+test('Failing API Test', { tag: '@API' }, async ({ homePage }) => {
+    await homePage.navigateToHomePageUser();
+    expect(2).toEqual(3);
+});
+
+test('Failing Regression Test', { tag: '@Regressions' }, async ({ homePage }) => {
     await homePage.navigateToHomePageUser();
     expect(2).toEqual(3);
 });
